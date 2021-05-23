33fa1b189a5c237ec50ce24159c5241f9e43cf4f
Ademir Mendonça <ademir_mendonca@hotmail.com>
2021-05-23 12:27:06 -0300

    Add project files.

---
+++ IdadeMediaURI1154/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace IdadeMediaURI1154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double idade;
+            double idadeTotal = 0;
+            int cont = 0;
+            double media = 0;
+
+            idade = double.Parse(Console.ReadLine());
+            while (idade >= 0)
+            {
+                cont = cont + 1;
+                idadeTotal = idadeTotal + idade;
+                idade = double.Parse(Console.ReadLine());                
+            }
+
+            
+            media = idadeTotal / cont;
+            Console.WriteLine(media.ToString("F2",CultureInfo.InvariantCulture));
+
+        }
+    }
+}
